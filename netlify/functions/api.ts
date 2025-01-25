3f5022dd83a0ca870a471f550bdfc1a0406543be
Dmytro Onishchenko <donischenko801@gmail.com>
2025-01-25 04:20:20 +0200

    WIP

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,10 +1,12 @@
 import express, { Router } from "express";
+import cors from "cors";
 import serverless from "serverless-http";
 
 import { generateRandomValue } from "../../src/utils/generateRandomValue";
 
-const api = express();
+const app = express();
 const router = Router();
+
 const INTERVAL = 10000;
 let latestValue = generateRandomValue();
 
@@ -12,8 +14,9 @@ setInterval(() => {
   latestValue = generateRandomValue();
 }, INTERVAL);
 
-router.get("/get-stats", (req: any, res: any) => res.json({ stats: "stats" }));
+app.use(cors());
+router.get("/get-stats", (req: any, res: any) => res.json(latestValue));
 
-api.use("/api/", router);
+app.use("/api/", router);
 
-export const handler = serverless(api);
+export const handler = serverless(app);
